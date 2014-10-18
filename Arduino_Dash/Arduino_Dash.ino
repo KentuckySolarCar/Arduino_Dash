3a0659aaffa3bb6c99cc98f83e1971a2552a9b32
Joshua Morgan <joshuamorganusa@gmail.com>
2014-10-18 19:47:49 -0400

    Hey! It works! Can write to both displays. It's true.

--- Arduino_Dash/Arduino_Dash.ino
+++ Arduino_Dash/Arduino_Dash.ino
@@ -1,1 +1,29 @@
+#include <Wire.h>
 
+#include <Adafruit_GFX.h>
+
+#include <Adafruit_LEDBackpack.h>
+
+//UK Solar Car Arduion Dash Program
+
+Adafruit_7segment sseg1 = Adafruit_7segment();
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Arduino_Dash");
+  
+  sseg1.begin(0x70);
+  
+}
+
+
+
+
+void loop()
+{
+ for(int i = 0; i < 100; i++)
+ {
+   sseg1.print(i, DEC);
+   delay(500);
+ }
+}
