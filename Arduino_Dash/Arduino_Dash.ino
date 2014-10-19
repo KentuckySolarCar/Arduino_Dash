118f3f7782a1e18bcd30a0163cf6050636998819
dcambron <daniel.cambron@uky.edu>
2014-10-19 17:45:04 -0400

    Added Serial to sseg communication and GPS
    The Arduino now accepts serial data with the following grammar:
    
    S=<float>\n     //speed to write to screen 1
    C=<float>\n    //current to write to screen 2
    
    where <float> is an ascii representation of a floating pt number
    
    GPS is also added. It spits out GPS data in NMEA sentences whenever it
    gets it from the GPS module.
    
    When the device hasn't heard anything from the PI, it sends out
    Arduino_Dash

--- Arduino_Dash/Arduino_Dash.ino
+++ Arduino_Dash/Arduino_Dash.ino
@@ -1,33 +1,45 @@
-//UK Solar Car Arduion Dash Program
-
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_LEDBackpack.h>
+#include <Adafruit_GPS.h>
+//the GPS library needs this event though it isn't used.
+#include <SoftwareSerial.h>
 
 //Definitions
-#define  sseg1_addr  0x70
-#define  sseg2_addr  0x71
-
+#define  sseg1_addr    0x70
+#define  sseg2_addr    0x71
+#define  ID_DELAY      500
+#define  DATA_TIMEOUT  3000
+#define  GPS_serial    Serial1
 
 //Set up 7 segment displays
 Adafruit_7segment sseg1 = Adafruit_7segment();
 Adafruit_7segment sseg2 = Adafruit_7segment();
 
+//Set up GPS
+Adafruit_GPS GPS(&GPS_serial);
+
 //Identification delay
-long identification_delay = 500;
 long last_identification= 0; 
+long last_data = 0;
 
+float val1 = 999.1; //values written to the displays
+float val2 = 999.2;
 
-
-int val1 = 3.45678;
-int val2 = 103.45;
-
-
+String serial_line = ""; //buffer for serial data
+boolean serial_line_flag = false;
 
 boolean pi_good = false; 
 
 
 void writeDashes(Adafruit_7segment); 
+
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  if (c) UDR0 = c; 
+  //char c = input_serial.read();
+  //Serial.print(c);
+}
 
 void setup()
 {
@@ -39,10 +51,19 @@ void setup()
   sseg1.begin(sseg1_addr);
   sseg2.begin(sseg2_addr);
   
+  //initialize input buffer
+  serial_line.reserve(200);
+  
+  //GPS module
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
+  GPS.sendCommand(PGCMD_NOANTENNA);
+  
+  //set up timer interrupt for GPS serial data
+  OCR0A = 0xAF;
+  TIMSK0 |= _BV(OCIE0A);
 }
-
-
-
 
 void loop()
 {
@@ -57,7 +78,7 @@ void loop()
     
     
    //Identify every identification_delay ms
-   if(millis() - last_identification > identification_delay)
+   if(millis() - last_identification > ID_DELAY)
    {
      Serial.println("Arduino_Dash");
      last_identification = millis();
@@ -66,21 +87,32 @@ void loop()
   
   else
   {
-    sseg1.print(val1, DEC);
-    sseg2.print(val2, DEC);
+    sseg1.print(val1);
+    sseg2.print(val2);
     sseg1.writeDisplay();
     sseg2.writeDisplay();
     sseg1.blinkRate(0);
     sseg2.blinkRate(0);
-   
-   
   }
   
   //When pi responds, set pi_good true
   if(!pi_good && Serial.available() > 0)
   {
     pi_good = true;
+    serial_line = "";
+    last_data=millis();
   }
+  
+  parseSerial();
+  
+  if(millis()-last_data > DATA_TIMEOUT)
+  {
+    pi_good = false;
+  }
+  
+
+  
+
   
   
   
@@ -161,9 +193,21 @@ void loop()
 
 
 
-
-
-
+void serialEvent() //special built-in arduino function
+{
+  //read serial data into buffer, set flag when EOL
+  while (Serial.available()) 
+  {
+    char inChar = (char)Serial.read(); 
+    serial_line += inChar;
+    
+    if (inChar == '\n') 
+    {
+      serial_line_flag = true;
+      //Serial.println("newline");
+    } 
+  } 
+}
 
 void writeDashes(Adafruit_7segment sseg)
 {
@@ -173,4 +217,28 @@ void writeDashes(Adafruit_7segment sseg)
   sseg.writeDigitRaw(0x04, 0b1000000);
   sseg.writeDisplay();
   
+}
+
+void parseSerial() 
+{
+  
+  if(serial_line_flag)
+  {
+    char buf[200];
+    last_data = millis();
+    serial_line.trim();
+    serial_line.toCharArray(buf,200);
+   //parse serial data
+   if(serial_line[0]=='S')
+   {
+     //sscanf(buf+2,"%f",&val1);
+     val1 = (float) atof(buf+2);
+   }
+   else if (serial_line[0]=='C')
+   {
+     val2 = (float) atof(buf+2);
+   } 
+  }
+    serial_line_flag = false;
+    serial_line = "";
 }
